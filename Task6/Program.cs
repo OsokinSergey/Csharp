7ba94a7053f9f208bae9d92351bf4a28f0af0e85
Osokin Sergey <osokin@intelgeotech.ru>
2022-07-10 19:31:27 +0300

    Add Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num%2==0)
+{
+   Console.WriteLine($"Число {num} является четным");     
+}
+else{
+   Console.WriteLine($"Число {num} не является четным");     
+}
