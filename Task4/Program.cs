168f6afb525d7e9f1baf09067a0d5f5d19ee40d4
Osokin Sergey <osokin@intelgeotech.ru>
2022-07-10 19:20:39 +0300

    Add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int maxNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+if (secondNum>maxNum)
+{
+    maxNum=secondNum;
+}
+Console.Write("Введите третье число: ");
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+if (thirdNum>maxNum)
+{
+    maxNum=thirdNum;
+}
+
+Console.WriteLine($"Максимальным является число {maxNum}");
