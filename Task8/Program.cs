e87241e1a2150c148b6834c2d07e674cb31d8f89
Osokin Sergey <osokin@intelgeotech.ru>
2022-07-10 20:01:24 +0300

    Add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int counter=1;
+string row="";
+
+while(counter<=num){
+    if(counter%2==0){
+        row = row + counter +  ",";
+     }
+    counter++; 
+}
+
+//При выводе на консоль удаляем последнюю запятую, т.е. последний символ. Для красоты.
+Console.WriteLine(row.Remove(row.Length-1));
+
+
