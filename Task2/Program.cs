29b0e50cb43a5ba7cf2d20a7f67baa76612bf115
Osokin Sergey <osokin@intelgeotech.ru>
2022-07-10 18:57:05 +0300

    init

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+if (firstNum==secondNum){
+    Console.WriteLine("Введенные числа равны");
+    return;
+}
+
+if (firstNum>secondNum)  
+{
+    Console.WriteLine($"max = {firstNum}, min = {secondNum}");
+}
+else
+{
+    Console.WriteLine($"max = {secondNum}, min = {firstNum}");
+}
+
