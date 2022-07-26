c11f59e237f7601f91744aec10ebbaf71e4994ed
Osokin Sergey <osa@intelgeotech.ru>
2022-07-26 16:36:06 +0300

    Add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿//int num=new Random().Next(10,10000000);
+int num=100;
+int result=ThirdDig(num);
+
+if(result!=-1) Console.WriteLine($"{num} -> {result}");
+else Console.WriteLine("Третьей цифры нет");
+
+int ThirdDig(int num)
+{   
+    if(num<100)
+    {
+        //Третьей цифры нет точно
+         num=-1;
+    }
+    else
+    {   //Последовательно уменьшаем число на порядок, пока оно не станет трехзначным
+        while (num > 999 )
+        {
+            num=num/10;
+        }
+    }
+    return num%10;
+}
