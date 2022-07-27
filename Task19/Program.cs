de6b689b1554bcf8989d8aa6b269cdaaf04a8c1c
Osokin Sergey <osa@intelgeotech.ru>
2022-07-27 10:48:05 +0300

    Add Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите пятизначное число ");
+int num=Convert.ToInt32(Console.ReadLine());
+if(num > 99999 || num < 10000 )
+{
+    Console.Write("Необходимо пятизначное число");
+    return;
+}
+
+if(Reverse(num)==num) Console.WriteLine($"{num} -> Да");
+else Console.WriteLine($"{num} -> Нет");
+
+int Reverse(int num) 
+{
+    //Начальная инициализация
+    int reverse=num%10;
+    //Теперь последняя цифра входного числа стала первой цифрой выходного числа.
+    //Далее организовываем цикл по оставшимся цифрам входного числа
+    while(num>9)
+    {   //Сбрасываем последний разряд у входного числа
+        num=num/10;
+        //Добавляем разряд к выходному числу и прибавляем остаток
+        reverse=reverse*10+num%10;
+    }
+    return reverse;
+}
