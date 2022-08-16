44f0a5db7b157d7eca71bba29ef82c606e1b01cc
Sergey Osokin <saosokin@gmail.com>
2022-08-16 23:17:24 +0300

    Add Task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int SumNumber(int number)
+{
+    //Инициализируем результирующую переменную последней цифрой входного числа
+    int result=number%10;
+    while(number>9) {
+        //"Обрезаем" последнюю цифру
+        number=number/10;
+        result=result+number%10;
+    }
+    return result;
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{number} -> {SumNumber(number)}");
